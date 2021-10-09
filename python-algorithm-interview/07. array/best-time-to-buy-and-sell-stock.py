65bff32d5930c96912a3b25e006cd11f67732a74
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-10-09 23:29:00 +0900

    Create best-time-to-buy-and-sell-stock.py

---
+++ python-algorithm-interview/07. array/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,26 @@
+# Solution1 : 브루트포스로 계산 - 타임아웃으로 실패
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_price = 0
+        
+        for i, price in enumerate(prices):
+            for j in range(i, len(prices)):
+                max_price = max(prices[j] - price, max_price)
+        
+        return max_price
+
+
+# Solution2 : 저점과 현재 값과의 차이의 계산
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        min_price = sys.maxsize
+        
+        # 최솟값과 최댓값을 계속 갱신
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+        
+        return profit
