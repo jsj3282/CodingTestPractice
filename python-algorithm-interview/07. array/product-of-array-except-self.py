23089688deb634a3a697314781ab2d4611515d4b
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-10-09 23:14:21 +0900

    Create product-of-array-except-self.py

---
+++ python-algorithm-interview/07. array/product-of-array-except-self.py
@@ -0,0 +1,17 @@
+# Solution1 : 왼쪽 곱셈 결과에 오른족 값을 차례대로 곱셈 - 나눗셈을 하지 않고 O(n)에 풀이하라
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        out = []
+        p = 1
+        # 왼쪽 곱셈
+        for i in range(0, len(nums)):
+            out.append(p)
+            p = p * nums[i]
+        p = 1
+        # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+        for i in range(len(nums) - 1, -1, -1):
+            out[i] = out[i] * p
+            p = p * nums[i]
+        
+        return out
