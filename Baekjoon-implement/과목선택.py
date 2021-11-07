05ecadaaeaf0b5b8b1afd0a4cade931785f81e4d
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-11-07 21:15:42 +0900

    Create 과목선택.py

---
+++ Baekjoon-implement/과목선택.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+ 
+science = [a, b, c, d]
+science.remove(min(science))
+ 
+print(sum(science) + max(e, f))
