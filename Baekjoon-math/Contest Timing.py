8094114ccba11abf9ea871cb3203d03fdc63f2e3
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-11-07 20:54:06 +0900

    Create Contest Timing.py

---
+++ Baekjoon-math/Contest Timing.py
@@ -0,0 +1,12 @@
+d, h, m = map(int, input().split())
+ 
+ 
+time1 = d * 24 * 60 + h * 60 + m
+time2 = 11 * 24 * 60 + 11 * 60 + 11
+ 
+time = time1 - time2
+ 
+if time2 > time1:
+    print(-1)
+else:
+    print(time)
