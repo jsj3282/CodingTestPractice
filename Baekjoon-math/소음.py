584bbfa3ad94be0f13a4112d830cb81166df536d
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-11-14 13:42:21 +0900

    Create 소음.py

---
+++ Baekjoon-math/소음.py
@@ -0,0 +1,10 @@
+first_num = int(input())
+operator = input()
+second_num = int(input())
+
+if operator == "+":
+    res = first_num + second_num
+else:
+    res = first_num * second_num
+
+print(res)
