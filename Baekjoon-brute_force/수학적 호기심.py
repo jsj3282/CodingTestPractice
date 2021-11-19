d08653f9ea95bb094f13210887f31cd57534c76f
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-11-19 21:01:59 +0900

    Create 수학적 호기심.py

---
+++ Baekjoon-brute_force/수학적 호기심.py
@@ -0,0 +1,10 @@
+import sys
+t = int(input())
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    cnt = 0
+    for i in range(1, n - 1):
+        for j in range(i + 1, n):
+            if (i ** 2 + j ** 2 + m) % (i * j) == 0:
+                cnt += 1
+    print(cnt)
