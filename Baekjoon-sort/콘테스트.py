a9225608c60f7e684076fc1efa4e1ed5a4374b9c
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-11-19 22:01:00 +0900

    Create 콘테스트.py

---
+++ Baekjoon-sort/콘테스트.py
@@ -0,0 +1,16 @@
+w = []
+k = []
+
+for _ in range(10):
+    w.append(int(input()))
+
+for _ in range(10):
+    k.append(int(input()))
+
+w.sort(reverse=True)
+k.sort(reverse=True)
+
+w_s = w[0] + w[1] + w[2]
+k_s = k[0] + k[1] + k[2]
+
+print(w_s, k_s)
