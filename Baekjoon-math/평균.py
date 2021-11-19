a6a4bb4313b300ba263ecf1e442da4c55a8ef145
jsj3282 <59247404+jsj3282@users.noreply.github.com>
2021-11-19 22:18:35 +0900

    Create 평균.py

---
+++ Baekjoon-math/평균.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+score = list(map(int, (input().split())))
+
+max_score = max(score)
+
+for i in range(len(score)):
+    score[i] = score[i] / max_score * 100
+
+avg = sum(score) / len(score)
+print(avg)
